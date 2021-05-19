5cb4db6d9c513de359c4f7ca91329ae051a45c71
lleka18 <63915090+lleka18@users.noreply.github.com>
2021-05-20 00:25:17 +0200

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,29 +1,150 @@
-//import 'dart:html';
+import 'package:epokalearner/profile.dart';
 import 'package:flutter/material.dart';
-import 'main.dart';
 
-void main() => runApp(MyApp());
-
-// ignore: camel_case_types
-class myApp extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: <Widget>[
-      Expanded(
-          child: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        primary: false,
-        children: <Widget>[
-          Card(
-              child: Column(children: <Widget>[
-            Icon(Icons.person),
-            Text("Profile"),
-          ]))
+      body: Column(
+        children: [
+          Container(
+            color: Colors.grey[800],
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3,
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+              image: new AssetImage('assets/images/logo.png'),
+              fit: BoxFit.fill,
+            )),
+          ),
+          Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                        child: GridView.count(
+                      crossAxisCount: 3,
+                      primary: false,
+                      children: <Widget>[
+                        FlatButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Profile(),
+                                ),
+                              );
+                            },
+                            child: Column(children: <Widget>[
+                              Icon(Icons.person,
+                                  size: 70, color: Colors.blue[300]),
+                              Text(
+                                "Profile",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.file_copy_outlined,
+                                  size: 70, color: Colors.yellow[600]),
+                              Text(
+                                "Courses",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.sticky_note_2,
+                                  size: 70, color: Colors.green),
+                              Text(
+                                "Interim",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.check_box_outlined,
+                                  size: 70, color: Colors.purple),
+                              Text(
+                                "Attendance",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.file_present,
+                                  size: 70, color: Colors.red),
+                              Text(
+                                "Documents",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.attach_money_rounded,
+                                  size: 70, color: Colors.blue[300]),
+                              Text(
+                                "Finance",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ])),
+                        FlatButton(
+                            onPressed: () {},
+                            child: Column(children: <Widget>[
+                              Padding(padding: EdgeInsets.all(5)),
+                              Icon(Icons.add_circle_outline,
+                                  size: 70, color: Colors.amber[700]),
+                              Text(
+                                "Add shortcut",
+                                textScaleFactor: 1.5,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ]))
+                      ],
+                    ))
+                  ])),
         ],
-      ))
-    ]));
+      ),
+    );
   }
 }
