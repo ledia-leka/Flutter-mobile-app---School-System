0e3616854260fb8ab77c4f2d363ab97db49383d7
lleka18 <63915090+lleka18@users.noreply.github.com>
2021-05-20 00:28:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:ui';
+// ignore: unused_import
+import 'package:epokalearner/home.dart';
+import 'package:epokalearner/profile.dart';
+import 'package:epokalearner/to-do.dart';
 import 'package:flutter/material.dart';
-import 'profile.dart';
-import 'login.dart';
+import 'package:epokalearner/to-do.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      /*     theme: ThemeData(
         primarySwatch: Colors.grey,
         primaryColor: Colors.black,
         brightness: Brightness.dark,
@@ -21,7 +25,7 @@ class MyApp extends StatelessWidget {
         accentIconTheme: IconThemeData(color: Colors.black),
         dividerColor: Colors.black12,
       ),
-/*
+*/
       theme: ThemeData(
         primarySwatch: Colors.grey,
         primaryColor: Colors.white,
@@ -30,7 +34,7 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.black,
         accentIconTheme: IconThemeData(color: Colors.white),
         dividerColor: Colors.white54,
-      ),*/
+      ),
       title: 'Epoka Feed',
       home: HomePage(),
     );
@@ -43,254 +47,158 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedItem = 0;
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = [];
+  Widget page1 = Home();
+  Widget page2 = TasksScreen();
+  @override
+  void initState() {
+    super.initState();
+
+    _widgetOptions = [page1, page2];
+  }
 
   @override
   Widget build(BuildContext context) {
+    final double phonewidth = MediaQuery.of(context).size.width / 4;
     return Scaffold(
-        appBar: new AppBar(
-            centerTitle: true,
-            shadowColor: Color.fromRGBO(255, 176, 50, 100),
-            title: new Text(
-              "Epoka e-Learner",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
+      appBar: new AppBar(
+          centerTitle: true,
+          shadowColor: Color.fromRGBO(255, 176, 50, 100),
+          title: new Text(
+            "Epoka e-Learner",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
             ),
-            backgroundColor: Color.fromRGBO(66, 135, 245, 100)),
-        drawer: new Drawer(
-            child: ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              currentAccountPicture:
-                  Icon(Icons.person_pin, size: 40, color: Colors.blueGrey),
-              accountName: new Text(
-                'Name',
-                style: TextStyle(color: Colors.grey[700], fontSize: 25),
-              ),
-              accountEmail: new Text(
-                'Email@epoka.edu.al',
-                style: TextStyle(color: Colors.grey[700], fontSize: 20),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.amberAccent,
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.verified_user),
-              title: Text('Profile'),
-              onTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NinjaCard()),
-                )
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.file_copy_outlined),
-              title: Text('Courses'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.sticky_note_2),
-              title: Text('Interim/Grades'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.check_box_outlined),
-              title: Text('Attendance'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.file_present),
-              title: Text('Documents'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.attach_money_outlined),
-              title: Text('Finance'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.calendar_today_rounded),
-              title: Text('Timetable'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.emoji_transportation),
-              title: Text('Transport'),
-              onTap: () => {Navigator.of(context).pop()},
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Logout'),
-              onTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Login()),
-                )
-              },
-            ),
-          ],
-        )),
-        body: SingleChildScrollView(
-          // rregullon viewn
-          child: Column(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: new BoxDecoration(
-                    color: Colors.grey[800],
-                    image: new DecorationImage(
-                      image: new AssetImage('assets/images/mm.jpg'),
-                      fit: BoxFit.fill,
-                    )),
-              ),
-              Container(
-                  padding: EdgeInsets.all(20),
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Expanded(
-                            child: GridView.count(
-                          crossAxisCount: 3,
-                          primary: false,
-                          children: <Widget>[
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => NinjaCard()),
-                                  );
-                                },
-                                child: Column(children: <Widget>[
-                                  Icon(Icons.person,
-                                      size: 70, color: Colors.blue[300]),
-                                  Text(
-                                    "Profile",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.file_copy_outlined,
-                                      size: 70, color: Colors.yellow[600]),
-                                  Text(
-                                    "Courses",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.sticky_note_2,
-                                      size: 70, color: Colors.green),
-                                  Text(
-                                    "Interim",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.check_box_outlined,
-                                      size: 70, color: Colors.purple),
-                                  Text(
-                                    "Attendance",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.file_present,
-                                      size: 70, color: Colors.red),
-                                  Text(
-                                    "Documents",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.attach_money_rounded,
-                                      size: 70, color: Colors.blue[300]),
-                                  Text(
-                                    "Finance",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ])),
-                            TextButton(
-                                onPressed: () {},
-                                child: Column(children: <Widget>[
-                                  Padding(padding: EdgeInsets.all(5)),
-                                  Icon(Icons.add_circle_outline,
-                                      size: 70, color: Colors.amber[700]),
-                                  Text(
-                                    "Add shortcut",
-                                    textScaleFactor: 1.5,
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ]))
-                          ],
-                        ))
-                      ])),
-            ],
+            textAlign: TextAlign.center,
           ),
-        ),
-        bottomNavigationBar: Row(
+          backgroundColor: Color.fromRGBO(66, 135, 245, 100)),
+      drawer: new Drawer(
+          child: ListView(
+        children: <Widget>[
+          new UserAccountsDrawerHeader(
+            currentAccountPicture:
+                Icon(Icons.person_pin, size: 40, color: Colors.blueGrey),
+            accountName: new Text(
+              'Name',
+              style: TextStyle(color: Colors.grey[700], fontSize: 25),
+            ),
+            accountEmail: new Text(
+              'Email@epoka.edu.al',
+              style: TextStyle(color: Colors.grey[700], fontSize: 20),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Profile(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.file_copy_outlined),
+            title: Text('Courses'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.sticky_note_2),
+            title: Text('Interim/Grades'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.check_box_outlined),
+            title: Text('Attendance'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.file_present),
+            title: Text('Documents'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.attach_money_outlined),
+            title: Text('Finance'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_today_rounded),
+            title: Text('Timetable'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.emoji_transportation),
+            title: Text('Transport'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      )),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.format_list_bulleted_sharp),
+            label: 'ToDo',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_sharp),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
+      /* Row(
           children: <Widget>[
             buildNavBar(Icons.home_rounded, 0),
             buildNavBar(Icons.favorite_sharp, 1),
             buildNavBar(Icons.format_list_bulleted_sharp, 2),
             buildNavBar(Icons.settings, 3),
           ],
-        ));
+        )); */
+    );
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+/*
   Widget buildNavBar(IconData icon, int index) {
     return GestureDetector(
       onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => TasksScreen()),
+        );
         setState(() {
           _selectedItem = index;
         });
@@ -311,6 +219,5 @@ class _HomePageState extends State<HomePage> {
             color: index == _selectedItem ? Colors.red : Colors.grey[350]),
       ),
     );
-  }
+  }*/
 }
-
