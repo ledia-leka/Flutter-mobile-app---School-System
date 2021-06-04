f414af1ecb8c68aaa8f59dcc8762a4ceef45ed1c
rominahoxha <82176555+rominahoxha@users.noreply.github.com>
2021-06-04 01:19:54 +0100

    Create transport.dart

---
+++ lib/transport.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+
+void main() {runApp(MyApp());}
+
+class MyApp extends StatefulWidget {
+  @override
+  Timetable createState() => Timetable();
+}
+
+class Timetable extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Transport Timetable'),
+          ),
+          body: ListView(children: <Widget>[
+            Center(
+                child: Text(
+                  'TIRANA CENTER - EPOKA CAMPUS',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,color: Colors.blue[700]),
+                )),
+            DataTable(
+              columns: [
+                DataColumn(label: Text(
+                    'No.',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'MON ',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'TUE ',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                
+                DataColumn(label: Text(
+                    'WED',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'THU',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'FRI ',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+                DataColumn(label: Text(
+                    'SAT',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                )),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('1')),
+                  DataCell(Text('7:40')),
+                  DataCell(Text('7:40')),
+                  DataCell(Text('7:40')),
+                  DataCell(Text('7:40')),
+                  DataCell(Text('7:40')),
+                  DataCell(Text('')),
+                  
+                  
+                ]), DataRow(cells: [
+                  DataCell(Text('staff only')),
+                  DataCell(Text('7:45')),
+                  DataCell(Text('7:45')),
+                  DataCell(Text('7:45')),
+                  DataCell(Text('7:45')),
+                  DataCell(Text('7:45')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                
+                DataRow(cells: [
+                  DataCell(Text('2')),
+                  DataCell(Text('9:40')),
+                  DataCell(Text('9:40')),
+                  DataCell(Text('9:40')),
+                  DataCell(Text('9:40')),
+                  DataCell(Text('9:40')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('3')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('4')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),DataRow(cells: [
+                  DataCell(Text('5')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('6')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('7')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('8')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('9')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('10')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('11')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('12')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                 DataRow(cells: [
+                  DataCell(Text('13')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                 DataRow(cells: [
+                  DataCell(Text('14')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+                 DataRow(cells: [
+                  DataCell(Text('15')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  DataCell(Text('')),
+                  
+                  
+                ]),
+               
+              ],
+            ),
+            
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            
+          
+            
+          ])
+      ),
+    );
+  }
+}
